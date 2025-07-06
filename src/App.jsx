6dc364f1c68cfc2b06c37f1dfe4820b441fc9e58
Themiya <themiyakaluarachchi@gmail.com>
2025-07-06 19:52:31 +0530

    feat : hero section is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 function App() {
   return (
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,6 +3,9 @@ import Image1 from "../../assets/hero/women.png"
 import Image2 from "../../assets/hero/shopping.png"
 import Image3 from "../../assets/hero/sale.png"
 import Slider from "react-slick"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 const ImageList = [
   {
@@ -29,39 +32,57 @@ const ImageList = [
 ]
 
 const Hero = () => {
+
+  var settings = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 800,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 10000,
+    cssEase: "ease-in-out",
+    pauseOnHover: false,
+    pauseOnFocus: true,
+  };
+
   return (
     <div className='relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200'>
       {/* background pattern */}
       <div className='h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 z-9'></div>
       {/* hero section */}
       <div className='container pb-8 sm:pb-0'>
-        <Slider>
-          <div>
-            <div className='grid grid-cols-1 sm:grid-cols-2'>
-              {/* text content section */}
-              <div className='relative z-10 flex flex-col justify-center order-2 gap-4 pt-12 text-center sm:pt-0 sm:text-left sm:order-1'> 
-                <h1 className='text-5xl font-bold sm:text-6xl lg:text-7xl'>Lorem ipsum dolor sit.</h1>
-                <p className='text-sm'>
-                  lorem afa kfalfa kalfkamma mdal akmdal dmadma ddmalda dkmalda adka paad lad dkad akdad amdamda?
-                </p>
-                <div>
-                    <button className='px-4 py-2 text-white duration-200 rounded-full bg-gradient-to-r from-primary to-secondary hover:scale-105'>
-                        Order Now
-                    </button>
-                </div>
-              </div> 
-              {/* image section */}
-              <div className='order-1 sm:order-2'> 
-                <div className='relative z-10'> 
-                  <img 
-                    src={Image1} 
-                    alt=""
-                    className='w-[300px] h-[300px] sm:h-[400px] sm:w-[450px] sm:scale-125 object-contain mx-auto'  
-                  />
+        <Slider {...settings}>
+          {ImageList.map((data) => (
+            <div>
+              <div className='grid grid-cols-1 sm:grid-cols-2'>
+                {/* text content section */}
+                <div className='relative z-10 flex flex-col justify-center order-2 gap-4 pt-12 text-center sm:pt-0 sm:text-left sm:order-1'> 
+                  <h1 className='text-5xl font-bold sm:text-6xl lg:text-7xl'>{data.title}</h1>
+                  <p className='text-sm'>
+                    {data.description}
+                  </p>
+                  <div>
+                      <button className='px-4 py-2 text-white duration-200 rounded-full bg-gradient-to-r from-primary to-secondary hover:scale-105'>
+                          Order Now
+                      </button>
+                  </div>
+                </div> 
+                {/* image section */}
+                <div className='order-1 sm:order-2'> 
+                  <div className='relative z-10'> 
+                    <img 
+                      src={data.img} 
+                      alt=""
+                      className='w-[300px] h-[300px] sm:h-[400px] sm:w-[450px] sm:scale-125 object-contain mx-auto'  
+                    />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          ))}
+         
         </Slider>
       </div>
     </div>
